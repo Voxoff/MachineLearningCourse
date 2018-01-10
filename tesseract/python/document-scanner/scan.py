07ae7575ccbc5d4af131ec7eaff0c4b156ef57ee
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-01-10 14:46:24 +0000

    tidy up files

--- tesseract/python/document-scanner/scan.py
+++ tesseract/python/document-scanner/scan.py
@@ -6,7 +6,7 @@ from pyimagesearch.transform import four_point_transform
 from pyimagesearch import imutils
 from skimage.filters import threshold_local
 import imutils
-import numpy as np
+# import numpy as np
 import argparse
 import cv2
 
@@ -31,11 +31,8 @@ edged = cv2.Canny(gray, 75, 200)
 
 # show the original image and the edge detected image
 # print "STEP 1: Edge Detection"
-cv2.imshow("Image", image)
-cv2.imshow("Edged", edged)
-# cv2.waitKey(0)
-# print "here"
-# cv2.destroyAllWindows()
+cv2.imshow("1. Image", image)
+cv2.imshow("2. Edged", edged)
 
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
@@ -55,11 +52,8 @@ for c in cnts:
 		break
 
 # show the contour (outline) of the piece of paper
-# print "STEP 2: Find contours of paper"
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-cv2.imshow("Outline", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow("3. Outline", image)
 
 # apply the four point transform to obtain a top-down
 # view of the original image
@@ -83,11 +77,10 @@ block_size = 35
 warped = threshold_local(warped_gray, block_size, offset=10)
 warped_adaptive = (warped_gray > warped).astype("uint8")*255
 
-# print("STEP 3: Apply perspective transform")
 
 cv2.imshow("Original", imutils.resize(orig, height=650))
 cv2.imshow("Scanned", imutils.resize(warped_adaptive, height=650))
-# cv2.imwrite("final.png", warped_adaptive)
+# cv2.imwrite("final.png", warped_adaptive) 
 
 
 # Rotate because it's always facing right, but keep corners
@@ -96,18 +89,5 @@ rotated = imutils.resize(rotated, height=650)
 cv2.imshow("Rotated (Correct)", rotated )
 cv2.imwrite("final.png", rotated)
 
-# rows,cols = warped_adaptive.shape
-# M = cv2.getRotationMatrix2D((cols/2, rows/2), 90, 1)
-# dst = cv2.warpAffine(warped_adaptive, M, (cols,rows))
-# cv2.imshow("Scanned", imutils.resize(dst, height=650))
-
-
-while True:
-    #key = cv2.waitKey(33) #this won't work
-    #key = 0xFF & cv2.waitKey(33) #this is ok
-    key = np.int16(cv2.waitKey(33)) #this is ok [2]
- 
-    if key == 27:
-        break
-    else:
-        print key, hex(key), key % 256
+cv2.waitKey(0)
+cv2.destroyAllWindows()
